ee711bb815d23f3059ee7411ffb0ca0e53732e49
Guilherme Viana <guilherme.viana@fabricainfo.com>
2022-01-28 00:25:06 -0300

    Implementando layout tela de login

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,28 @@
+import {
+    Text,
+    Image,
+    View,
+    TouchableOpacity,
+    TouchableOpacityProps
+} from 'react-native';
+
+import DiscordImg from '../../assets/discord.png';
+import { styles } from './styles';
+
+type Props = TouchableOpacityProps & {
+    title: string;
+}
+
+export function ButtonIcon({ title, activeOpacity } : Props){
+    return(
+        <TouchableOpacity style={styles.container} activeOpacity={activeOpacity}>
+            <View style={styles.iconWrapper}>
+                <Image source={DiscordImg} style={styles.icon} />
+            </View>
+
+            <Text style={styles.title}>
+                { title }
+            </Text>
+        </TouchableOpacity>
+    );
+}
